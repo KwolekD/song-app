81383fdba077ae7e5c04e91ed04fa35360deddf6
KwolekD <damiankwolek1@gmail.com>
2025-05-27 18:05:49 +0200

    final

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 // Importujemy Express 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const multer = require('multer');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 // Dane - lista albumów 
 const albums = [
-    { id: 1, band: "Metallica", title: "Master of Puppets", year: 1986,genre: "Rock", img: null },
-    { id: 2, band: "Metallica", title: "Ride the Lightning", year: 1984,genre: "Rock", img: null  },
-    { id: 3, band: "AC/DC", title: "Back in Black", year: 1980,genre: "Rock", img: null  },
-    { id: 4, band: "AC/DC", title: "Highway to Hell", year: 1979,genre: "Rock", img: null  },
-    { id: 5, band: "Iron Maiden", title: "The Number of the Beast",genre: "Rock", year: 1982, img: null  },
-    { id: 6, band: "Iron Maiden", title: "Powerslave", year: 1984,genre: "Rock", img: null  }
+    { id: 1, band: "Metallica", title: "Master of Puppets", year: 1986, genre: "Rock", img: null },
+    { id: 2, band: "Metallica", title: "Ride the Lightning", year: 1984, genre: "Rock", img: null },
+    { id: 3, band: "AC/DC", title: "Back in Black", year: 1980, genre: "Rock", img: null },
+    { id: 4, band: "AC/DC", title: "Highway to Hell", year: 1979, genre: "Rock", img: null },
+    { id: 5, band: "Iron Maiden", title: "The Number of the Beast", genre: "Rock", year: 1982, img: null },
+    { id: 6, band: "Iron Maiden", title: "Powerslave", year: 1984, genre: "Rock", img: null }
 ];
 
 const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,'uploads/');
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
@@ -30,9 +32,11 @@ app.use(express.json());
 
 app.use(cors()); // Middleware do obsługi CORS
 
-app.use('/uploads', express.static('uploads')); 
+const uploadsPath = path.join(__dirname, 'uploads');
 
-app.put('albums/:id/img', upload.single('img'), (req, res) => {
+app.use('/uploads', express.static(uploadsPath));
+
+app.put('/albums/:id/img', upload.single('img'), (req, res) => {
     const id = parseInt(req.params.id);
     const album = albums.find(album => album.id === id);
     if (!album) {
@@ -41,7 +45,19 @@ app.put('albums/:id/img', upload.single('img'), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: "Brak pliku obrazu." });
     }
-    album.img = `/uploads/${req.file.filename}`;
+    // Sprawdzenie czy jest folder uploads
+
+
+    if (album.img) {
+        // Jeśli album już ma obraz, usuwamy stary plik
+        const oldImgPath = path.join(uploadsPath, album.img.split('/').pop());
+        fs.unlink(oldImgPath, (err) => {
+            if (err) {
+                console.error("Błąd podczas usuwania starego obrazu:", err);
+            }
+        });
+    }
+    album.img = `http://localhost:3000/uploads/${req.file.filename}`;
     res.json(album);
 });
 
@@ -68,20 +84,20 @@ app.post('/albums', (req, res) => {
     if (!band || !title || !year || !genre) {
         return res.status(400).json({ message: "Brak wymaganych danych." });
     }
-    const newAlbum = { id: albums[albums.length-1].id+1, band, title, year, genre, cover };
+    const newAlbum = { id: albums[albums.length - 1].id + 1, band, title, year, genre, cover };
     albums.push(newAlbum);
     res.status(201).json(newAlbum);
 });
 // Endpoint do aktualizacji albumu
 app.put('/albums/:id', (req, res) => {
-    const id = parseInt(req.params.id); 
+    const id = parseInt(req.params.id);
     const { title, genre, cover } = req.body;
-    const album = albums.find(album => album.id === id); 
+    const album = albums.find(album => album.id === id);
     if (!album) {
         return res.status(404).json({ message: "Album nie znaleziony." });
     }
-    if (title) album.title = title; 
-    if (genre) album.genre = genre; 
+    if (title) album.title = title;
+    if (genre) album.genre = genre;
     if (cover) album.cover = cover;
     res.json(album);
 });
@@ -92,11 +108,24 @@ app.delete('/albums/:id', (req, res) => {
         (index === -1) {
         return res.status(404).json({ message: "Album nie znaleziony." });
     }
+    // Sprawdzenie, czy album ma przypisany obrazek
+    if (albums[index].img) {
+        const fs = require('fs');
+        const imgPath = path.join(uploadsPath, albums[index].img.split('/').pop());
+        fs.unlink(imgPath, (err) => {
+            if (err) {
+                console.error("Błąd podczas usuwania obrazu:", err);
+            }
+        });
+    }
     albums.splice(index, 1);
     res.json({ message: "Album usunięty." });
 });
 // Uruchomienie serwera 
 app.listen(port,
     () => {
+        if (!fs.existsSync(uploadsPath)) {
+            fs.mkdirSync(uploadsPath, { recursive: true });
+        }
         console.log(`Serwer REST API działa na http://localhost:${port}`);
     });
--- my-album-app/src/App.js
+++ my-album-app/src/App.js
@@ -1,11 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+
+
 
 export default function AlbumList() {
   const [albums, setAlbums] = useState([]);
   const [band, setBand] = useState("");
-  const [newAlbum, setNewAlbum] = useState({ band: "", title: "", year: "" , genre: "" });
+  const [newAlbum, setNewAlbum] = useState({ band: "", title: "", year: "", genre: "", img: undefined });
+  const AlbumImgUpload = ({ albumId }) => {
+    const fileInputRef = useRef(null);
 
+    const uploadAlbumImg = async (albumId, file) => {
+      const formData = new FormData();
+      formData.append('img', file);
+      const response = await fetch(`http://localhost:3000/albums/${albumId}/img`, {
+        method: 'PUT',
+        body: formData,
+      })
+        .then(async response => {
+          const data = await response.json();
+          if (!response.ok) {
+            console.error('Błąd podczas przesyłania okładki:', data.message);
+            throw new Error('Błąd serwera');
+          }
+          else {
+            console.log('Okładka przesłana pomyślnie:', data);
+            // Aktualizacja lokalnego stanu albumów
+            const updatedAlbums = albums.map(album => album.id === albumId ? { ...album, img: data.img }
+              : album);
+            updateLocalStorage(updatedAlbums);
+            return data;
+
+          }
+        })
+        .catch(error => {
+          console.error('Błąd podczas przesyłania okładki:', error);
+        });
+    };
+    const handleUpload = async (e) => {
+      const file = e.target.files[0];
+      if (!file) return;
+
+      try {
+        await uploadAlbumImg(albumId, file);
+      } catch (error) {
+        console.error('Błąd podczas przesyłania okładki:', error);
+      }
+    };
+
+    return (
+      <div>
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleUpload}
+          accept="image/*"
+          style={{ display: 'none' }}
+        />
+        <button
+          onClick={() => fileInputRef.current.click()}
+          className="upload-button"
+        >
+          Dodaj okładkę
+        </button>
+      </div>
+    );
+  };
 
   const updateLocalStorage = (updatedAlbums) => {
     setAlbums(updatedAlbums);
@@ -37,36 +97,43 @@ export default function AlbumList() {
   };
 
 
+
   const addAlbum = () => {
     fetch("http://localhost:3000/albums", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newAlbum)
     })
-      .then(async response => 
-        {
-          const data = await response.json();
-          if (!response.ok) {
-            // Obsługa błędu (np. 400)
-            console.warn("Błąd z serwera:", data.message);
-            return;
-          }
-          else {
-            setNewAlbum({ band: "", title: "", year: "", genre: "" });
-            updateLocalStorage([...albums, data]);
-          }
-        })
-        .catch(error => console.error("Błąd dodawania albumu:", error));
+      .then(async response => {
+        const data = await response.json();
+        if (!response.ok) {
+          // Obsługa błędu (np. 400)
+          console.warn("Błąd z serwera:", data.message);
+          return;
+        }
+        else {
+          setNewAlbum({ band: "", title: "", year: "", genre: "" });
+          updateLocalStorage([...albums, data]);
+        }
+      })
+      .catch(error => console.error("Błąd dodawania albumu:", error));
   };
-  const updateAlbum = (id, newTitle) => {
+  const updateAlbum = (id) => {
     fetch(`http://localhost:3000/albums/${id}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title: newTitle })
+      body: JSON.stringify(newAlbum)
     })
       .then(response => response.json())
       .then(() => {
-        const updatedAlbums = albums.map(album => album.id === id ? { ...album, title: newTitle }
+        const updatedAlbums = albums.map(album => album.id === id ? {
+          id: id,
+          band: newAlbum.band || album.band,
+          title: newAlbum.title || album.title,
+          year: newAlbum.year || album.year,
+          genre: newAlbum.genre || album.genre,
+          img: album.img
+        }
           : album);
         updateLocalStorage(updatedAlbums);
       })
@@ -87,7 +154,7 @@ export default function AlbumList() {
   useEffect(() => {
     const storedAlbums = localStorage.getItem("albums");
     if (storedAlbums) {
-      setAlbums(JSON.parse(storedAlbums)  );
+      setAlbums(JSON.parse(storedAlbums));
     } else {
       fetch("http://localhost:3000/albums")
         .then(response => response.json())
@@ -110,10 +177,10 @@ export default function AlbumList() {
           placeholder="Wpisz nazwę zespołu"
           value={band}
           onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                fetchBandAlbums();
-              }
+            if (e.key === 'Enter') {
+              fetchBandAlbums();
             }
+          }
           }
           onChange={(e) => setBand(e.target.value)}
           className="input"
@@ -148,7 +215,7 @@ export default function AlbumList() {
             setNewAlbum({ ...newAlbum, year: e.target.value })}
           className="input"
         />
-        <input 
+        <input
           type="text"
           placeholder="Gatunek"
           value={newAlbum.genre}
@@ -156,23 +223,31 @@ export default function AlbumList() {
             setNewAlbum({ ...newAlbum, genre: e.target.value })}
           className="input"
         />
+
+      </div>
+      <div className="search-buttons">
         <button onClick={addAlbum} disabled={
           !newAlbum.band || !newAlbum.title || !newAlbum.year || !newAlbum.genre
         } className="button green">Dodaj Album</button>
+        <button onClick={() => setNewAlbum({ band: "", title: "", year: "", genre: "" })} className="button gray">Wyczyść</button>
       </div>
+
 
       <ul className="album-list">
         {albums.map(album => (
           <li key={album.id} className="album-item">
-            <div className="album-cover-container">
-              <img src={album.cover || "https://placehold.co/100"} alt={`${album.band} - ${album.title}`} className="album-cover" />
+            <div className="album-img-container">
+              <img src={album.img || "https://placehold.co/100"} alt={`${album.band} - ${album.title}`} className="album-img" />
             </div>
             <span>
-              <strong>{album.band}</strong> - {album.title} ({album.year})
+              <strong>{album.band}</strong> - {album.title} ({album.year}) - {album.genre}
             </span>
             <div className="action-buttons">
-              <button onClick={() => updateAlbum(album.id)} className="button yellow">Edytuj</button>
+              <button onClick={() => updateAlbum(album.id)} disabled={
+                newAlbum.band == "" && newAlbum.title == "" && newAlbum.year == "" && newAlbum.genre == ""
+              } className="button yellow">Edytuj</button>
               <button onClick={() => deleteAlbum(album.id)} className="button red">Usuń</button>
+              <AlbumImgUpload albumId={album.id} />
             </div>
           </li>
         ))}
